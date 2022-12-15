60597203afd7f9e32884f3c23086d950db7b63f2
18nfurfari <18nfurfari@gmail.com>
2022-12-15 01:07:18 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,83 @@
+import mercari
+import time
+import discord_notify as dn
+from datetime import datetime
+import numpy as np
+
+
+def main():
+    # def search(keywords, sort="created_time", order="desc", status="on_sale", limit=120):
+    search_query = "のカービィ ときめきクレーン"
+    notifier = dn.Notifier(
+        "https://discord.com/api/webhooks/1051654616133148732/ON0N3XSvxxYMkbR929yQ99d2Hiyq99b9tyyj3We5A8e0YHtiIARh2gkkYgkARuy5Mwqy")
+
+    # forming initial "old" list
+    print("-----Old list formed-----", datetime.now().strftime("%m/%d/%Y %H:%M:%S"))
+    old_List, old_count = perform_search(search_query)
+    print("Old list count: ", old_count)
+
+    # sleep for 10 seconds to slow down next call
+    time.sleep(10)
+
+
+    while True:
+        # forming new list of items
+        print("-----Checking for new items-----", datetime.now().strftime("%m/%d/%Y %H:%M:%S"))
+        new_List, new_count = perform_search(search_query)
+        print("New list count: ", new_count)
+
+        # resetting old list because an item sold
+        if old_count - new_count > 0:
+            print("Item sold, reformatting old list")
+            old_List, old_count = perform_search(search_query)
+
+            print("-----Old list formed-----", datetime.now().strftime("%m/%d/%Y %H:%M:%S"))
+            print("Old list count: ", old_count)
+
+        # sending discord notifications if new listings
+        elif old_count - new_count < 0:
+            notifier.send("----------New Items!----------", print_message=True)
+
+            # finding items in new_List NOT in old_List
+            listings = np.setdiff1d(new_List, old_List)
+
+            # seding discord notification for each new listing
+            for item in listings:
+                discord_message = item.productName + " " + item.productURL \
+                                  + "               " + item.imageURL + " "
+                notifier.send(discord_message, print_message=True)
+                time.sleep(1)
+
+            # reset old list
+            print("New item listed, reformatting old list")
+            old_List, old_count = perform_search(search_query)
+            print("-----Old list formed-----", datetime.now().strftime("%m/%d/%Y %H:%M:%S"))
+            print("Old list count: ", old_count)
+
+
+        # refreshing search every 10 minutes
+        time.sleep(600)
+
+
+def perform_search(search_query):
+    item_list = []
+    item_count = 0
+    search_success = False
+
+    while not search_success:
+        try:
+            for item in mercari.search(search_query):
+                item_list.append(item)
+                item_count += 1
+            search_success = True
+        except:
+            print("API error, searching again...")
+            item_list = []
+            item_count = 0
+            time.sleep(10)
+
+    return item_list, item_count
+
+
+if __name__ == '__main__':
+    main()
